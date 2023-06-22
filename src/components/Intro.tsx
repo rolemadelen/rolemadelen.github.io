d2843bbc24d8ec78479b13f779493ba65ee1cc0f
Jii Yoo <jiiyoo@tuta.io>
2023-06-22 14:30:34 -0400

    portfolio almost done
    - right section can be scrolled from anywhere
    - rensponise UI
    - need refactoring

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Intro: React.FC = () => {
   return (
-    <>
-      <div className='mb-8'>
-        <p className=''>
-          I'm a front-end developer currently residing in Kentucky, passionate about crafting minimalist and simple digital experiences.
-        </p>
-        <p>
-          When I'm not in front of a computer screen, I'm probably walking my puppy, drawing on my iPad, playing taiko or practicing shinobue.
-        </p>
-      </div>
-    </>
+    <div className='font-light mx-16 sm:mx-20'>
+      <p>
+        With a dedication to web development spanning two years, I've cultivated my skills in this
+        ever-evolving field. Fueled by boundless curiosity,
+        I thrive on continuous learning to stay at the forefront of technology.
+      </p>
+      <p>
+        When I'm not immersed in coding, you'll likely find me enjoying the simple
+        pleasures of life – walking my puppy, creating art on my iPad,
+        playing taiko, or practicing my skills on the shinobue flute.
+      </p>
+    </div>
   )
 }
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,55 +2,64 @@ import React from 'react';
 
 const Skills: React.FC = () => {
   return (
-    <>
-      <div className='skills mb-8'>
-        <h2 className='font-bold'>Skills</h2>
-        <p>
-          Over the course of two years, I have been honing my skills in web development, and my insatiable curiosity fuels my desire to constantly expand my knowledge in this rapidly changing field.
-        </p>
-        <div className='flex justify-between'>
-          <ul className='languages'>
-            <li className='title'>Languages</li>
-            <li>HTML 5</li>
-            <li>CSS 3</li>
-            <li>CSS Preprocessors</li>
-            <li>JavaScript (ES6)</li>
-            <li>TypeScript</li>
-            <li>C / C++</li>
-          </ul>
-          <ul className='frameworks'>
-            <li className='title'>Frameworks</li>
-            <li>React.js</li>
-            <li>Next.js</li>
-            <li>Express.js</li>
-            <li>Jekyll</li>
-            <li>TailwindCSS</li>
-            <li>DaisyUI</li>
-          </ul>
-          <ul className='tools'>
-            <li className='title'>Tools</li>
-            <li>Bash</li>
-            <li>Git Version Control</li>
-            <li>GitHub & Bitbucket</li>
-            <li>Jira</li>
-            <li>AWS</li>
-            <li>MongoDB</li>
-            <li>Postman</li>
-            <li>Netlify / Vercel / Heroku</li>
-            <li>Figma</li>
-          </ul>
-        </div>
-        <p>
-        </p>
-        <ul className='have-exp-with'>
-          <li className='title'>Have Experience</li>
-          <li className='note'>
-            * I have hands-on experience with these from past projects and can swiftly adapt and familiarize myself with their syntax and usage.
-          </li>
-          <li>Java / Python / Ruby / Rails / PHP / jQuery / Vue.js</li>
+    <div className='skills mb-8 mx-16 sm:mx-20' id='skills'>
+      <h2>Skills</h2>
+      <div className='flex flex-wrap m-auto justify-between w-[75vw] lg:w-[40vw] xl:w-full'>
+        <ul className='languages mx-2 mb-8 w-36'>
+          <li className='title'>Languages</li>
+          <li>HTML 5</li>
+          <li>CSS 3</li>
+          <li>CSS Preprocessors</li>
+          <li>JavaScript (ES6)</li>
+          <li>TypeScript</li>
+          <li>C / C++</li>
         </ul>
+        <ul className='frameworks mx-2 mb-8 w-36'>
+          <li className='title'>Frameworks</li>
+          <li>React.js</li>
+          <li>Next.js</li>
+          <li>Express.js</li>
+          <li>Jekyll</li>
+          <li>TailwindCSS</li>
+          <li>DaisyUI</li>
+        </ul>
+        <ul className='tools mx-2 w-36'>
+          <li className='title'>Tools</li>
+          <li>Bash</li>
+          <li>Git Version Control</li>
+          <li>GitHub</li>
+          <li>Bitbucket</li>
+          <li>Jira</li>
+          <li>AWS</li>
+          <li>MongoDB</li>
+          <li>Postman</li>
+          <li>Netlify / Vercel / Heroku</li>
+          <li>Figma</li>
+        </ul>
+
+        {/*
+        <ul className='have-used mx-2 w-36'>
+          <li className='title'>Have Used</li>
+          <li>Java</li>
+          <li>Python</li>
+          <li>Ruby</li>
+          <li>Ruby on Rails</li>
+          <li>PHP</li>
+          <li>jQuery</li>
+          <li>Vue.js</li>
+        </ul>
+        */}
+
+        <div className='have-exp-with mt-8'>
+          <div className='note text-sm text-gray-500 mb-4'>
+            * I have hands-on experience with skills below from past projects and can swiftly adapt and familiarize myself with their syntax and usage.
+          </div>
+          <div className='text-[0.9rem] text-[#555]'>Java / Python / Ruby / Rails / PHP / jQuery / Vue.js</div>
+        </div>
       </div>
-    </>
+      <p>
+      </p>
+    </div>
   )
 }
 
