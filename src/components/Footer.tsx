bb077acf2d4a97bc023025d30c457a4ef60ed9cf
Madelen Yoo <rolemadelen@tuta.io>
2023-06-23 18:58:19 -0400

    footer cut off from iphone xr 4
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <>
-      <div className='font-light text-sm text-center mt-16 mb-12'>
+      <div className='font-light text-sm text-center mt-16 mb-12 min-h-[9rem]'>
         Designed & Developed by <a href="mailto:eu.jii@protonmail.com" className='underline underline-offset-4 decoration-from-font'>Jii Eu</a> @ 2023
         <div className='my-4'>૮ ・ﻌ・ა</div>
       </div>
