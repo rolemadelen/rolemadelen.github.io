3e3f4a9c7a606c5c4f8aa4749937382f70155188
rolemadelen <rolemadelen@pm.me>
2023-06-23 06:03:20 -0400

    footer? desc added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact: React.FC = () => {
   return (
     <>
-      <div className='contact  font-light text-sm text-center mt-20 mb-20 lg:mb-8'>
+      <div className='contact  font-light text-sm text-center mt-20 mb-16 lg:mb-8'>
         Designed & Developed by <a href="mailto:eu.jii@protonmail.com" className='link'>Jii Eu</a> @ 2023
       </div>
     </>
