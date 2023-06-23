cdfcaa7f8262f9d8d5c91954f2aa4e9e281e857c
Jii Yoo <jiiyoo@tuta.io>
2023-06-23 13:48:29 -0400

    right section structure modified

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Intro: React.FC = () => {
   return (
-    <div className='font-light mx-8'>
+    <div className='text-md font-light mx-8 lg:mt-[7.5rem]'>
       <p>
         With a dedication to web development spanning two years, I've cultivated my skills in this
         ever-evolving field. Fueled by boundless curiosity,
@@ -12,6 +12,9 @@ const Intro: React.FC = () => {
         When I'm not immersed in coding, you'll likely find me enjoying the simple
         pleasures of life – walking my <span className='puppy'>pu</span><span className='puppy2'>ppy</span>, creating art on my iPad,
         playing taiko, or practicing my skills on the shinobue flute.
+      </p>
+      <p>
+        I'm open for <b>remote</b> opportunities.
       </p>
     </div>
   )
