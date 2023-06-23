5dbe927fd8556760f021ed97da89d95256cb717b
Jii Yoo <jiiyoo@tuta.io>
2023-06-23 18:17:32 -0400

    mobile top button ... fixed?

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <>
-      <div className='font-light text-sm text-center mt-20 mb-16 lg:mb-8'>
-        Designed & Developed by <a href="mailto:eu.jii@protonmail.com" className='underline underline-offset-4 decoration-from-font'>Jii Eu</a> @ 2023
+      <div className='relative pt-16 pb-8 flex flex-col items-center'>
+        <div className='font-light text-sm text-center mb-8'>
+          Designed & Developed by <a href="mailto:eu.jii@protonmail.com" className='underline underline-offset-4 decoration-from-font'>Jii Eu</a> @ 2023
+        </div>
+        <a href="#top" className='text-xs text-gray-500 rounded-md border-gray-300 border-[1px] w-10 h-10 flex justify-center items-center my-4 lg:hidden'>top</a>
       </div>
     </>
   )
