c95b32c9a2d687cb0cc0d71baca426f3c2c3d0c9
rolemadelen <rolemadelen@pm.me>
2023-06-24 08:36:25 -0400

    projects img changed gif

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,41 +7,46 @@ const Projects: React.FC = () => {
     <>
       <div className='experience mb-24 mx-8' id='projects'>
         <h2 className='title'>Projects</h2>
+        <div className='flex items-center ml-4 lg:justify-end text-sm mb-8'>
+          <span className='inline-block bg-[#e3edff] border-2 border-blue-200 w-16 rounded-md h-6'></span>
+          <span className='inline-block mx-2'> — </span>
+          <span className='inline-block'>Primarily Utilized</span>
+        </div>
         <div className='project'>
           <Project
-            imgsrc={'/src/assets/aikomicare.jpg'}
+            imgsrc={'/src/assets/aikomicare.gif'}
             title={'Aikomiケア Website'}
-            skillsHighlight={['Vite', 'JavaScript', 'HTML', 'SCSS']}
-            skills={['PHP', 'AWS Lightsail']}
+            skillsHighlight={['JavaScript', 'HTML', 'SCSS']}
+            skills={['Vite', 'PHP', 'AWS Lightsail']}
             link={'https://aikomicare.com/'}>
             <div className='text-sm'>
               A webiste designed to introduce users to Aikomi Care and step-by-step guidance throughout their entire service experience.
             </div>
           </Project>
           <Project
-            imgsrc={'/src/assets/minimalistblog.jpg'}
+            imgsrc={'/src/assets/minimalistblog.gif'}
             title={'Minimalist | Personal Blog'}
-            skillsHighlight={['TypeScript', 'TailwindCSS', 'Styled Component', 'Vercel']}
-            skills={['Next.js']}
+            skillsHighlight={['Next.js', 'TypeScript', 'TailwindCSS', 'Styled Component']}
+            skills={['Vercel']}
             link={'https://www.bepersistent.dev/'}>
             <div className='text-sm'>
               Created a minimalist-inspired blog featuring clean aesthetics and a focus on simplicity, providing users with a distraction-free reading experience.
             </div>
           </Project>
           <Project
-            imgsrc={'/src/assets/artlog.jpg'}
+            imgsrc={'/src/assets/artlog.gif'}
             title={'Artlog'}
-            skillsHighlight={['Node.js', 'React.js', 'Express.js', 'Node.js', 'TypeScript', 'SCSS', 'Axios']}
-            skills={['MongoDB', 'Redux']}
+            skillsHighlight={['React.js', 'Redux', 'TypeScript', 'SCSS', 'MongoDB', 'Axios']}
+            skills={['Node.js', 'Express.js']}
             link={'https://artlogs.netlify.app/'}>
             <div className='text-sm'>
               Created Artlog as a personal endeavor, curating a visual diary of my daily outfits as a hobby artist, fostering creativity and self-expression in the realm of minimalist fashion.
             </div>
           </Project>
           <Project
-            imgsrc={'/src/assets/portfolio.jpg'}
+            imgsrc={'/src/assets/portfolio.gif'}
             title={'www.jiieu.com'}
-            skillsHighlight={['React.js', 'TypeScript', 'TailwindCSS', 'SCSS']}
+            skillsHighlight={['React.js', 'React Hooks', 'TypeScript', 'TailwindCSS', 'SCSS']}
             skills={[]}
             link={'https://www.jiieu.com'}
           >
