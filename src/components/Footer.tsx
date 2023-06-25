a302ce35dcae6ec7cb5cdc65bdad5a3ee55d4eee
Madelen Yoo <rolemadelen@tuta.io>
2023-06-24 23:51:50 -0400

    easter eggs
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,18 @@ interface Props {
 const Footer: React.FC<Props> = ({ handleOnClick }) => {
 
   return (
-    <div className='font-light text-sm text-center mt-16 mb-12'>
-      Designed & Developed by <a href="mailto:eu.jii@protonmail.com" className='underline underline-offset-4 decoration-from-font'>Jii Eu</a> @ 2023
-      <div className='relative my-4 top' onClick={handleOnClick}><span>૮</span> ・ﻌ・<span>ა</span></div>
-    </div>
+    <>
+      <div className='font-light text-sm text-center mt-16 mb-12'>
+        Designed & Developed by <a href="mailto:eu.jii@protonmail.com" className='underline underline-offset-4 decoration-from-font'>Jii Eu</a> @ 2023
+        <div className='relative my-4 top' onClick={handleOnClick}><span>૮</span> ・ﻌ・<span>ა</span></div>
+      </div>
+      <div className='easter-egg text-xs text-gray-300 mx-auto text-center'>
+        ~ easter eggs ~
+        <div className='text-[0.7rem]'>
+          There are two real images of my puppy hidden in this page.
+        </div>
+      </div>
+    </>
   )
 }
 
