69d076729206fe56f8e0d34a717d831082f2f882
rolemadelen <rolemadelen@pm.me>
2023-06-25 08:19:26 -0400

    arrow icon updated

--- src/components/AllProjects.tsx
+++ src/components/AllProjects.tsx
@@ -31,7 +31,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2023</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/portfolio" target="_blank" rel="noopener noreferrer">
-                  Portfolio Website <span className='arrow'>⇗</span>
+                  Portfolio Website <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -48,7 +48,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2023</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/minimalist-blog" target="_blank" rel="noopener noreferrer">
-                  Minimalist Blog <span className='arrow'>⇗</span>
+                  Minimalist Blog <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -66,7 +66,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2023</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/artlog" target="_blank" rel="noopener noreferrer">
-                  Artlog <span className='arrow'>⇗</span>
+                  Artlog <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -87,7 +87,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2023</td>
               <td className='text-sm'>
                 <a href="https://aikomicare.com/" target="_blank" rel="noopener noreferrer">
-                  Aikomiケア Website <span className='arrow'>⇗</span>
+                  Aikomiケア Website <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -106,7 +106,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2023</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/ghost-vatar" target="_blank" rel="noopener noreferrer">
-                  Ghostvatar <span className='arrow'>⇗</span>
+                  Ghostvatar <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -124,7 +124,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2022</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/blog-v2" target="_blank" rel="noopener noreferrer">
-                  Personal Blog v3 <span className='arrow'>⇗</span>
+                  Personal Blog v3 <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -142,7 +142,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2022</td>
               <td className='text-sm'>
                 <a href="https://github.com/euisblue/nextjs-blog-template" target="_blank" rel="noopener noreferrer">
-                  Multilingual Blog Template <span className='arrow'>⇗</span>
+                  Multilingual Blog Template <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -161,7 +161,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2021</td>
               <td className='text-sm'>
                 <a href="https://github.com/euisblue/bluelog" target="_blank" rel="noopener noreferrer">
-                  Personal Blog v2 <span className='arrow'>⇗</span>
+                  Personal Blog v2 <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -180,7 +180,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2021</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/til-logger" target="_blank" rel="noopener noreferrer">
-                  TIL Logger <span className='arrow'>⇗</span>
+                  TIL Logger <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -199,7 +199,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2020</td>
               <td className='text-sm'>
                 <a href="https://github.com/euisblue/euisblue.github.io" target="_blank" rel="noopener noreferrer">
-                  Personal Blog v1 <span className='arrow'>⇗</span>
+                  Personal Blog v1 <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -215,7 +215,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2020</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/boj-solvedac" target="_blank" rel="noopener noreferrer">
-                  Baekjoon Online Judge CLI <span className='font-light'>(8k+ downloads)</span> <span className='arrow'>⇗</span>
+                  Baekjoon Online Judge CLI <span className='font-light'>(8k+ downloads)</span> <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
@@ -230,7 +230,7 @@ const AllProjects: React.FC = () => {
               <td className='text-sm'>2017</td>
               <td className='text-sm'>
                 <a href="https://github.com/rolemadelen/chess" target="_blank" rel="noopener noreferrer">
-                  Text-based Chess <span className='arrow'>⇗</span>
+                  Text-based Chess <span className='arrow'></span>
                 </a>
               </td>
               <td className='built-with hidden sm:block'>
