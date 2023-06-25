d9b9de2b3a10f957c1d13b06e242291accc8b9a4
Jii Yoo <jiiyoo@tuta.io>
2023-06-25 08:20:53 -0400

    cv link modifed from hash - browser routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CV from './pages/cv.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/">
           <Route index element={<App />} />
           <Route path="cv" element={<CV />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
