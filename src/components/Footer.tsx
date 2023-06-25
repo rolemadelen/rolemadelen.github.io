8df0770a1193e796d54eaafbb3bc0a8c060f4035
Madelen Yoo <rolemadelen@tuta.io>
2023-06-24 23:57:28 -0400

    easter eggs
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,8 +12,8 @@ const Footer: React.FC<Props> = ({ handleOnClick }) => {
         Designed & Developed by <a href="mailto:eu.jii@protonmail.com" className='underline underline-offset-4 decoration-from-font'>Jii Eu</a> @ 2023
         <div className='relative my-4 top' onClick={handleOnClick}><span>૮</span> ・ﻌ・<span>ა</span></div>
       </div>
-      <div className='easter-egg text-xs text-gray-300 mx-auto text-center'>
-        ~ easter eggs ~
+      <div className='easter-egg text-xs text-gray-300 mx-auto text-center my-8'>
+        ~ easter eggs (PC only)~
         <div className='text-[0.7rem]'>
           There are two real images of my puppy hidden in this page.
         </div>
