89369871eda2545270f5c3d82ff1c8e25a24fc50
Madelen Yoo <rolemadelen@tuta.io>
2024-01-05 19:47:30 -0500

    name updated
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,67 @@
-import './App.css'
-import { useCallback, useEffect, useRef } from 'react'
-import Skills from './components/Skills'
-import Intro from './components/Intro'
-import Experience from './components/Experience'
-import Projects from './components/Projects'
-import Footer from './components/Footer'
-import Languages from './components/Languages'
+import './App.css';
+import { useCallback, useEffect, useRef } from 'react';
+import Skills from './components/Skills';
+import Intro from './components/Intro';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import Languages from './components/Languages';
 
 const App: React.FC = () => {
-  const madelenRef = useRef<HTMLDivElement | null>(null)
-  const videoRef = useRef<HTMLVideoElement | null>(null)
-  const scrollRef = useRef<HTMLDivElement | null>(null)
-  const nameRef = useRef<HTMLHeadingElement | null>(null)
-  const roleRef = useRef<HTMLHeadingElement | null>(null)
-  const innerRef = useRef<HTMLDivElement | null>(null)
+  const madelenRef = useRef<HTMLDivElement | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const nameRef = useRef<HTMLHeadingElement | null>(null);
+  const roleRef = useRef<HTMLHeadingElement | null>(null);
+  const innerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const bg = document.querySelector('.background') as HTMLDivElement
-    const splashName = document.querySelector('.splash-name') as HTMLHeadingElement
-    const splashPortfolio = document.querySelector('.splash-portfolio') as HTMLHeadingElement
+    const bg = document.querySelector('.background') as HTMLDivElement;
+    const splashName = document.querySelector(
+      '.splash-name'
+    ) as HTMLHeadingElement;
+    const splashPortfolio = document.querySelector(
+      '.splash-portfolio'
+    ) as HTMLHeadingElement;
 
     setTimeout(() => {
-      splashName.style.opacity = '1.0'
+      splashName.style.opacity = '1.0';
 
       setTimeout(() => {
-        splashPortfolio.style.opacity = '1.0'
-      }, 500)
-    }, 500)
+        splashPortfolio.style.opacity = '1.0';
+      }, 500);
+    }, 500);
 
     setTimeout(() => {
-      splashName.style.opacity = '0.0'
-      splashPortfolio.style.opacity = '0.0'
-      if (videoRef.current) videoRef.current.style.opacity = '1.0'
-      if (madelenRef.current) madelenRef.current.style.opacity = '1.0'
-      bg.style.width = "90%"
+      splashName.style.opacity = '0.0';
+      splashPortfolio.style.opacity = '0.0';
+      if (videoRef.current) videoRef.current.style.opacity = '1.0';
+      if (madelenRef.current) madelenRef.current.style.opacity = '1.0';
+      bg.style.width = '90%';
 
       setTimeout(() => {
-        splashName.style.display = 'none'
-        splashPortfolio.style.display = 'none'
-      }, 800)
-    }, 3000)
-
-  }, [])
+        splashName.style.display = 'none';
+        splashPortfolio.style.display = 'none';
+      }, 800);
+    }, 3000);
+  }, []);
 
   const handleOnScroll = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
     if (videoRef && videoRef.current) {
-      videoRef && videoRef.current && videoRef.current?.pause()
-      scrollRef.current = e.currentTarget as HTMLDivElement
+      videoRef && videoRef.current && videoRef.current?.pause();
+      scrollRef.current = e.currentTarget as HTMLDivElement;
 
-      const rate = (scrollRef.current.scrollHeight - document.body.scrollHeight) / videoRef.current.duration
-      videoRef.current.currentTime = scrollRef.current.scrollTop / rate
+      const rate =
+        (scrollRef.current.scrollHeight - document.body.scrollHeight) /
+        videoRef.current.duration;
+      videoRef.current.currentTime = scrollRef.current.scrollTop / rate;
     }
-  }, [])
+  }, []);
 
   const handleOnWheel = useCallback((e: React.WheelEvent) => {
     if (scrollRef && scrollRef.current)
-      scrollRef.current.scrollTop += (e.deltaY / 2)
-  }, [])
+      scrollRef.current.scrollTop += e.deltaY / 2;
+  }, []);
 
   const handleOnClick = useCallback(() => {
     if (!scrollRef || !scrollRef.current) return;
@@ -64,76 +69,133 @@ const App: React.FC = () => {
 
     scrollRef.current.scrollTop = 0;
     innerRef.current.scrollTop = 0;
-  }, [])
+  }, []);
 
   const handleOnContextMenu = useCallback(() => {
-    return false
-  }, [])
+    return false;
+  }, []);
 
   return (
     <>
       <div className='splash absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-3xl text-align text-center'>
-        <h1 className='splash-name opacity-0 transition-opacity duration-1000'>Jii Eu</h1>
-        <h2 className='splash-portfolio opacity-0 duration-1000 transition-opacity text-gray-600'>Portfolio</h2>
-      </div >
+        <h1 className='splash-name opacity-0 transition-opacity duration-1000'>
+          Jii Yoo
+        </h1>
+        <h2 className='splash-portfolio opacity-0 duration-1000 transition-opacity text-gray-600'>
+          Portfolio
+        </h2>
+      </div>
 
-      <div className='relative' onWheel={handleOnWheel}>
-        <video onContextMenu={handleOnContextMenu} controls={false} muted={true} loop={false} autoPlay={false} ref={videoRef} className='opacity-0 z-[-1] fixed left-0 bottom-0 h-full transition-opacity duration-1000 ease-in-out'>
-          <source src='/src/assets/madelen-video.webm' type='video/webm; codecs="vp8.0, vorbis"' />
-          <source src='/src/assets/madelen-video.mp4' type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"' />
+      <div
+        className='relative'
+        onWheel={handleOnWheel}>
+        <video
+          onContextMenu={handleOnContextMenu}
+          controls={false}
+          muted={true}
+          loop={false}
+          autoPlay={false}
+          ref={videoRef}
+          className='opacity-0 z-[-1] fixed left-0 bottom-0 h-full transition-opacity duration-1000 ease-in-out'>
+          <source
+            src='/src/assets/madelen-video.webm'
+            type='video/webm; codecs="vp8.0, vorbis"'
+          />
+          <source
+            src='/src/assets/madelen-video.mp4'
+            type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'
+          />
         </video>
 
-        <div className='madelen opacity-0 fixed left-0 bottom-0 bg-cover h-screen w-screen z-[-1] transition-opacity duration-1000 ease-in-out' ref={madelenRef}></div>
+        <div
+          className='madelen opacity-0 fixed left-0 bottom-0 bg-cover h-screen w-screen z-[-1] transition-opacity duration-1000 ease-in-out'
+          ref={madelenRef}></div>
 
         <div className='background w-0 rounded-sm max-w-screen-2xl m-auto fixed shadow-2xl z-[-1] transition-width duration-1000 ease-in-out overflow-hidden'>
-          <div className='inner flex flex-col lg:flex-row h-screen overflow-scroll' ref={innerRef}>
+          <div
+            className='inner flex flex-col lg:flex-row h-screen overflow-scroll'
+            ref={innerRef}>
             <aside className='aside left main header mx-8 pt-4 lg:pt-8 lg:pb-20 flex flex-col justify-between'>
               <header>
-                <h1 className='text-5xl font-light text-gray-800' ref={nameRef}>
-                  Jii Eu
+                <h1
+                  className='text-5xl font-light text-gray-800'
+                  ref={nameRef}>
+                  Jii Yoo
                 </h1>
-                <h2 className='mb-12 text-md text-gray-500' ref={roleRef}>
+                <h2
+                  className='mb-12 text-md text-gray-500'
+                  ref={roleRef}>
                   Front-End Engineer
                 </h2>
                 <div className='text-md mb-32 max-w-[35rem] text-gray-700'>
-                  I'm a front-end developer currently residing in Kentucky — passionate about crafting creative and simple digital experiences.
+                  I'm a front-end developer currently residing in Kentucky —
+                  passionate about crafting creative and simple digital
+                  experiences.
                 </div>
               </header>
               <nav className='nav text-sm border-b-[1px] border-[#ccc] lg:border-0 pb-20 lg:pb-10 text-gray-800'>
                 <ul className='nav-list mb-8'>
                   <li>
-                    <a className='nav-item' href='https://github.com/rolemadelen' target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a
+                      className='nav-item'
+                      href='https://github.com/rolemadelen'
+                      target='_blank'
+                      rel='noopener noreferrer'>
+                      GitHub
+                    </a>
                   </li>
                   <li>
-                    <a className='nav-item' href='https://www.linkedin.com/in/jiieu/' target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    <a
+                      className='nav-item'
+                      href='https://www.linkedin.com/in/jiieu/'
+                      target='_blank'
+                      rel='noopener noreferrer'>
+                      LinkedIn
+                    </a>
                   </li>
                   <li>
-                    <a className='nav-item' href='https://www.bepersistent.dev/' target="_blank" rel="noopener noreferrer">Blog</a>
+                    <a
+                      className='nav-item'
+                      href='https://www.bepersistent.dev/'
+                      target='_blank'
+                      rel='noopener noreferrer'>
+                      Blog
+                    </a>
                   </li>
                 </ul>
                 <div className='flex flex-col'>
-                  <a href="/cv" className='cv nav-item'>
+                  <a
+                    href='/cv'
+                    className='cv nav-item'>
                     <span>૮</span> ･ ﻌ･<span>ა</span> CV
                   </a>
-                  <a className='nav-item' href="mailto:eu.jii@protonmail.com">
-                    eu.jii@protonmail.com
+                  <a
+                    className='nav-item'
+                    href='mailto:rolemadelen@pm.me'>
+                    rolemadelen@pm.me
                   </a>
                 </div>
               </nav>
             </aside>
-            <div className='content lg:overflow-scroll h-auto pt-16 lg:py-8 mb-20' onScroll={handleOnScroll} ref={scrollRef}>
+            <div
+              className='content lg:overflow-scroll h-auto pt-16 lg:py-8 mb-20'
+              onScroll={handleOnScroll}
+              ref={scrollRef}>
               <Intro />
               <Experience />
               <Skills />
-              <Languages nameRef={nameRef} roleRef={roleRef} />
+              <Languages
+                nameRef={nameRef}
+                roleRef={roleRef}
+              />
               <Projects />
               <Footer handleOnClick={handleOnClick} />
             </div>
           </div>
         </div>
-      </div >
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -1,45 +1,69 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
 
 interface Props {
-  nameRef: React.RefObject<HTMLHeadingElement>
-  roleRef: React.RefObject<HTMLHeadingElement>
+  nameRef: React.RefObject<HTMLHeadingElement>;
+  roleRef: React.RefObject<HTMLHeadingElement>;
 }
 
 const Languages: React.FC<Props> = ({ nameRef, roleRef }) => {
-  const langRef = useRef<HTMLDivElement | null>(null)
+  const langRef = useRef<HTMLDivElement | null>(null);
 
   const handleOnClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (!langRef || !langRef.current) return;
     if (!nameRef || !nameRef.current) return;
     if (!roleRef || !roleRef.current) return;
 
-
-    const flag = e.currentTarget.innerText
+    const flag = e.currentTarget.innerText;
 
     if (flag === '🇰🇷') {
-      langRef.current.innerText = '언어 능력'
-      nameRef.current.innerText = '유지이'
-      roleRef.current.innerText = '프론트엔드 개발자'
+      langRef.current.innerText = '언어 능력';
+      nameRef.current.innerText = '유지이';
+      roleRef.current.innerText = '프론트엔드 개발자';
     } else if (flag === '🇯🇵') {
-      langRef.current.innerText = '語学力'
-      nameRef.current.innerText = '劉 ジー'
-      roleRef.current.innerText = 'フロントエンド エンジニア'
+      langRef.current.innerText = '語学力';
+      nameRef.current.innerText = '劉 ジー';
+      roleRef.current.innerText = 'フロントエンド エンジニア';
     } else {
-      langRef.current.innerText = 'Languages'
-      nameRef.current.innerText = 'Jii Eu'
-      roleRef.current.innerText = 'Front-End Developer'
+      langRef.current.innerText = 'Languages';
+      nameRef.current.innerText = 'Jii Yoo';
+      roleRef.current.innerText = 'Front-End Developer';
     }
-  }
+  };
   return (
-    <div className='skills mb-24 mx-8 text-center' id='skills'>
-      <h2 id="lang" className='title' ref={langRef}>Languages</h2>
+    <div
+      className='skills mb-24 mx-8 text-center'
+      id='skills'>
+      <h2
+        id='lang'
+        className='title'
+        ref={langRef}>
+        Languages
+      </h2>
       <div className='grid grid-rows-3 grid-cols-3 gap-4 text-center inline-grid text-[0.9rem] text-[#555]'>
-        <div>Korean</div> <div className='hover:cursor-pointer' onClick={handleOnClick}>🇰🇷</div> <div>Native</div>
-        <div>English</div><div className='hover:cursor-pointer' onClick={handleOnClick}>🇺🇸</div> <div>Fluent </div>
-        <div>Japanese</div> <div className='hover:cursor-pointer' onClick={handleOnClick}>🇯🇵</div><div> Intermediate</div>
+        <div>Korean</div>{' '}
+        <div
+          className='hover:cursor-pointer'
+          onClick={handleOnClick}>
+          🇰🇷
+        </div>{' '}
+        <div>Native</div>
+        <div>English</div>
+        <div
+          className='hover:cursor-pointer'
+          onClick={handleOnClick}>
+          🇺🇸
+        </div>{' '}
+        <div>Fluent </div>
+        <div>Japanese</div>{' '}
+        <div
+          className='hover:cursor-pointer'
+          onClick={handleOnClick}>
+          🇯🇵
+        </div>
+        <div> Intermediate</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Languages
+export default Languages;
